e375954b41a6fc0f37089fe5c1e560ac1e0764ae
d0ughnat <nledesma35@gmail.com>
2025-08-26 14:32:44 +0800

    hero glass effect add

--- resources/js/landingpage/HeroSection.tsx
+++ resources/js/landingpage/HeroSection.tsx
@@ -1,10 +1,30 @@
 import styles from './HeroSection.module.css';
+import Img from '/home/natty/BWC/public/images/heroPic.png';
+
 
 export default function HeroSection() {
   return (
     <section className={styles.heroSection} aria-label="Hero">
-      <h1 className={styles.heroTitle}>Welcome to Our Service</h1>
-      <p className={styles.heroSubtitle}>Your trusted partner for professional solutions.</p>
+      <div className={styles.heroGlassEffect}>
+        <div className={styles.heroLeftContent}>
+          <h1>
+            <span className={styles.heroTitle}>Invisible Window <br />Cleaning </span> 
+            <span className={styles.heroTitle}> – Crystal-Clear <br />Results Across Bristol</span>
+          </h1>
+          <h2 className={styles.heroSubtitle}>Friendly, Local, Fully Insured Window Cleaners for Homes & Businesses</h2>
+          <ul className={styles.heroSubtitle}>
+            <li>Regular 6-weekly cleans or one-off visits</li>
+            <li>Guaranteed finish.</li>
+            <li>Text reminders before every clean</li>
+          </ul>
+          <button className={styles.heroButton}> <span className={styles.heroButtonIcon}>Get a Free Quote</span></button>
+          <h3 className={styles.heroBottomText}>Fast replies | No obligation</h3>
+        </div>
+        <div className={styles.heroRightContent}>
+          <img src={Img} alt="hero image" />
+        </div>
+      </div>
+      
     </section>
   );
 }
