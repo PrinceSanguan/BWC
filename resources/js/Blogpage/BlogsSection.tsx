b120fc4a3061f94178b0cdd66e0086c67a317894
Jaii <122984752+johnlouisejizdeortega@users.noreply.github.com>
2025-09-11 23:58:01 +0800

    feat: sanitize blog descriptions by replacing h1 tags for improved HTML rendering

--- resources/js/Blogpage/BlogsSection.tsx
+++ resources/js/Blogpage/BlogsSection.tsx
@@ -25,6 +25,12 @@ export default function BlogsSection({ blogs }: { blogs: Blog[] }) {
     };
   }, [openBlog]);
 
+  // Helper to remove or demote h1 tags from blog.description
+  function sanitizeDescription(html: string) {
+    // Replace <h1> with <span> or <h2>, or remove entirely
+    return html.replace(/<h1(.*?)>([\s\S]*?)<\/h1>/gi, '<span$1>$2</span>');
+  }
+
   return (
     <section className={styles.servicesSection} aria-label="Our Blogs" style={{ background: '#fff' }}>
       <div className={styles.servicesGrid}>
@@ -35,7 +41,7 @@ export default function BlogsSection({ blogs }: { blogs: Blog[] }) {
             <div
               className={styles.serviceDesc + ' ' + styles.clamp3}
               style={{ textAlign: 'left' }}
-              dangerouslySetInnerHTML={{ __html: blog.description }}
+              dangerouslySetInnerHTML={{ __html: sanitizeDescription(blog.description) }}
             />
             <Link href={`/blogs/${blog.slug}`} className={styles.readMoreButton}>Read more</Link>
           </article>
